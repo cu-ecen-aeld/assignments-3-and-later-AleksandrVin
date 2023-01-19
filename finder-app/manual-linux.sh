eba025a08ea7748ea76183514c293aa0f1d89201
Aleksandr Vinogradov <vinogradov.alek@gmail.com>
2023-01-20 00:59:43 +0300

    just to move to server

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -7,11 +7,12 @@ set -u
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_VERSION=v5.1.10
+KERNEL_VERSION=v6.1.6
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+BUILD_OPS="ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}"
 
 if [ $# -lt 1 ]
 then
@@ -34,13 +35,24 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # TODO: Add your kernel build steps here 
+    echo $BUILD_OPS
+    echo "mrproper" 
+    make $BUILD_OPS mrproper
+    echo "defconfig"
+    make $BUILD_OPS defconfig
+    echo "all"
+    make -j $(nproc) $BUILD_OPS all
+    echo "modules"
+    make $BUILD_OPS modules
+    echo "dtbs"
+    make $BUILD_OPS dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp -v $OUTDIR/linux-stable/arch/$ARCH/boot/Image $OUTDIR
 
 echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
@@ -49,6 +61,13 @@ fi
 
 # TODO: Create necessary base directories
 
+mkdir ${OUTDIR}/rootfs/
+cd ${OUTDIR}/rootfs
+mkdir bin dev etc home lib proc sbin sys tmp usr var
+mkdir usr/bin usr/lib usr/sbin
+mkdir -p var/log
+sudo chown -R root:root *
+
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
@@ -56,11 +75,14 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make $BUILD_OPS defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+sudo make $BUILD_OPS CONFIG_PREFIX="${OUTDIR}/rootfs" install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
@@ -68,7 +90,10 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
 
+
 # TODO: Make device nodes
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # TODO: Clean and build the writer utility
 
