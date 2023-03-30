11329d78ab9c824c7d700a86deb5913562936985
Aleksandr Vinogradov <vinogradov.alek@gmail.com>
2023-03-31 00:38:40 +0300

    fix path to writer

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -55,7 +55,7 @@ echo "Removing the old writer utility and compiling as a native application"
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
